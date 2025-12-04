fe7c7fc446f3fc693266d7d38fed8c253d41cebb
Tomas Carnecky <tomc@caurea.org>
2025-12-04 15:48:54 +0100

    GitHub Output Armor . .

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,30 @@
 use flakers::{parse_entry, parse_header};
-use nom::{Parser, multi::many0};
+use nom::{Parser,multi::many0};
 use std::io::{self, Read};
+use clap::Parser as ArgsParser;
+use rand::{distr::Alphabetic, Rng};
+
+#[derive(ArgsParser)]
+struct Args {
+    /// If set, output is wrapped in an armor, allowing it to be piped
+    /// directly into $GITHUB_OUTPUT.
+    #[arg(long)]
+    github_output_armor: Option<String>,
+}
+
+struct GitHubOutputArmor {
+    delimiter: String,
+}
+
+impl Drop for GitHubOutputArmor {
+    fn drop(&mut self) {
+        println!("{}", self.delimiter);
+    }
+}
 
 fn main() {
+    let args = Args::parse();
+
     let mut input = String::new();
     #[allow(clippy::expect_used)]
     io::stdin()
@@ -23,6 +45,19 @@ fn main() {
             eprintln!("Failed to parse entries: {}", e);
             std::process::exit(1);
         }
+    };
+
+    let _guard = if let Some(output_key) = &args.github_output_armor {
+        let delimiter = rand::rng()
+            .sample_iter(&Alphabetic)
+            .take(20)
+            .map(char::from)
+            .collect::<String>();
+
+        println!("{}<<{}", output_key, &delimiter);
+        Some(GitHubOutputArmor { delimiter })
+    } else {
+        None
     };
 
     println!("<details><summary>Raw output</summary><p>");
